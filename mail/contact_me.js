708c94d3f55cb34c85ccbcf5421258c466b521a2
Akeen11 <aartkeen@ut.utm.edu>
2018-11-06 15:26:18 -0600

    attempted conversion of php to js

--- mail/contact_me.js
+++ mail/contact_me.js
@@ -1,4 +1,3 @@
-<?php
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
@@ -6,7 +5,7 @@ if(empty($_POST['name'])      ||
    empty($_POST['message'])   ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-   echo "No arguments Provided!";
+   echo("No arguments Provided!");
    return false;
    }
    
@@ -18,9 +17,8 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'aartkeen@ut.utm.edu'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form. Here are the details: Name: $name Email: $email_address Phone: $phone Message: $message";
 $headers = "From: noreply@akeen11.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$headers = "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
-return true;         
-?>
+return true;
